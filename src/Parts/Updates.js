31aa4aca0e0982b461a0871bdf0262e3b8006056
Anthony Corcutt <acorcutt@gmail.com>
2020-09-27 22:59:45 +0100

    updates layout

--- src/Parts/Updates.js
+++ src/Parts/Updates.js
@@ -21,26 +21,30 @@ function Snippet({ content }) {
 }
 export default function Updates({ updates, pathname }) {
   return (
-    <div className="bb-grid bb-gap-16 lg:bb-grid-cols-2 lg:bb-col-gap-5 lg:bb-row-gap-12">
+    <div className="bb-space-y-8">
       {updates &&
         updates.map((item, index) => (
-          <div key={index}>
-            <p className="bb-text-sm bb-leading-5 bb-text-gray-500">
+          <div key={index} className="bb-grid bb-grid-cols-3 bb-border-t bb-border-gray-100 bb-pt-6">
+            <p className="bb-col-span-1 bb-text-2xl bb-leading-5 bb-text-gray-500 bb-font-thin">
               <time dateTime="2020-03-16">{new Date(item.created).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</time>
             </p>
-            <a href="#" className="bb-block">
-              <h3 className="bb-mt-2 bb-text-xl bb-leading-7 bb-font-semibold bb-text-gray-900">{item.title}</h3>
+            <div className="bb-col-span-2">
+              <h3 className="bb-text-xl bb-leading-7 bb-font-semibold bb-text-gray-900">
+                <Link to={`${pathname}/updates/${item.slug}`} className="hover:bb-underline">
+                  {item.title}
+                </Link>
+              </h3>
               <p className="bb-mt-3 bb-text-base bb-leading-6 bb-text-gray-500">
                 <Snippet content={item.content} />
               </p>
-            </a>
-            <div className="bb-mt-3">
-              <Link
-                to={`${pathname}/updates/${item.slug}`}
-                className="bb-text-base bb-leading-6 bb-font-semibold bb-text-indigo-600 hover:bb-text-indigo-500 bb-transition bb-ease-in-out bb-duration-150"
-              >
-                Read full update
-              </Link>
+              <div className="bb-mt-3">
+                <Link
+                  to={`${pathname}/updates/${item.slug}`}
+                  className="hover:bb-underline bb-text-base bb-leading-6 bb-font-semibold bb-text-indigo-600 hover:bb-text-indigo-500 bb-transition bb-ease-in-out bb-duration-150"
+                >
+                  Read full update
+                </Link>
+              </div>
             </div>
           </div>
         ))}
