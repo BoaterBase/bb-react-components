1372ea23953122e4490c48fff7d436c0eb9299a3
Anthony Corcutt <acorcutt@gmail.com>
2021-01-11 21:41:21 +0000

    add gallery preview

--- src/Parts/Updates.js
+++ src/Parts/Updates.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Markdown from 'markdown-to-jsx';
 import Link from '../Link';
+import Gallery from '../parts/Gallery';
 
 /** Create a snippet from a content block */
 function Snippet({ content }) {
@@ -19,11 +20,24 @@ function Snippet({ content }) {
         ))
     : null;
 }
-export default function Updates({ updates, pathname }) {
+
+function PreviewGallery({ content }) {
+  return content
+    ? content
+        .filter(({ kind }) => ['media'].includes(kind))
+        .slice(0, 1)
+        .map(({ items }, index) => (
+          <div className="bb-mt-2">
+            <Gallery key={index} media={items} layout="grid" limit={3} />
+          </div>
+        ))
+    : null;
+}
+export default function Updates({ updates, pathname, limit = 9 }) {
   return (
     <div className="bb-space-y-8">
       {updates &&
-        updates.map((item, index) => (
+        updates.slice(0, limit).map((item, index) => (
           <div key={index} className="bb-grid bb-grid-cols-3 bb-border-t bb-border-gray-100 bb-pt-6">
             <p className="bb-col-span-3 md:bb-col-span-1 bb-mb-2 bb-text-xl md:bb-text-2xl bb-leading-5 bb-text-gray-500 bb-font-extralight">
               <time dateTime="2020-03-16">{new Date(item.created).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</time>
@@ -34,6 +48,7 @@ export default function Updates({ updates, pathname }) {
                   {item.title}
                 </Link>
               </h3>
+              <PreviewGallery content={item.content} />
               <p className="bb-mt-3 bb-text-base bb-leading-6 bb-text-gray-500">
                 <Snippet content={item.content} />
               </p>
