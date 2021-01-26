b1cdb33fb77d3fce0b2f668992a1123cca941846
Anthony Corcutt <acorcutt@gmail.com>
2021-01-26 19:23:12 +0000

    fix header test

--- src/data/convertWebsite.js
+++ src/data/convertWebsite.js
@@ -15,7 +15,7 @@ export default function convertWebsite() {
         theme: data.theme,
         menu: data.menu,
         footer: data.footer,
-        header: data?.header?.info && normalizeMedia(data.header),
+        header: data?.header?.info?.resource_type && normalizeMedia(data.header),
         contact: data.contact,
         message: data.message,
         copyright: data.copyright,
